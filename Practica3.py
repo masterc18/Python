2c90da18291f244f6e3ef6953f61a3ad34a64a02
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-10-09 16:05:42 -0600

    Practica de tiempo transcurridos entre years

---
+++ Practica3.py
@@ -0,0 +1,7 @@
+firstYear = int(input("Write the first year: "))
+secondYear = int(input("Write the secon year: "))
+
+result = (secondYear - firstYear)*12
+Time = round(result)
+
+print(f"Betwwen {firstYear} and {secondYear} has passed {Time} months")
